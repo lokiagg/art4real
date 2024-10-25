57865b91989a970984f756506f8a2844cb617a5b
lokiagg <1306426768@qq.com>
2024-10-25 16:48:13 +0800

    Update Common.h

--- include/Common.h
+++ include/Common.h
@@ -103,7 +103,7 @@ constexpr int64_t kPerThreadRdmaBuf  = rdmaBufferSize * define::GB / MAX_APP_THR
 constexpr int64_t kPerCoroRdmaBuf    = kPerThreadRdmaBuf / MAX_CORO_NUM;
 
 // Cache (MB)
-constexpr int kIndexCacheSize = 4096;
+constexpr int kIndexCacheSize = 8192;
 
 // KV
 constexpr uint32_t maxkeyLen = 128;   
