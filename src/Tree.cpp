ed30b6d9fdfd8d34b5937698014f9d48a829b45a
lokiagg <1306426768@qq.com>
2024-10-25 14:54:24 +0800

    Update Tree.cpp

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -362,7 +362,7 @@ if(parent_type ==0)  //一个内部节点    1.继续往下找  2. 有一个空�
     assert(bhdr.depth !=0);
     depth = bhdr.depth + bhdr.partial_len;
     auto partial = get_partial(k, depth);  //获取需要匹配的关键字 应该是缓冲节点的深度再加上partial len
-    auto loop_start = std::chrono::high_resolution_clock::now();
+/*    auto loop_start = std::chrono::high_resolution_clock::now();
     GlobalAddress leaf_addrs[256];
     GlobalAddress leaves_ptr[256];
     memset(leaf_addrs,0,256*sizeof(GlobalAddress));
@@ -438,7 +438,7 @@ if(parent_type ==0)  //一个内部节点    1.继续往下找  2. 有一个空�
               goto insert_finish;
           }
         }
-    }
+    }*/
     //3.4 still have empty slot  不存在部分键相同的情况  有的话 则往下找 否则放空位 
   //  if(bhdr.count_1+bhdr.count_2 < 256)
    // {
