a23f09b15ec1b15acfc08b0f62d374b2f8c04d85
lokiagg <1306426768@qq.com>
2024-10-26 16:09:37 +0800

    Update Node.h

--- include/Node.h
+++ include/Node.h
@@ -445,7 +445,7 @@ public:
       // this->records[i].val = records[i].val;
     // }
         //  memcpy(this->records,records,sizeof(BufferEntry)*256);
-    std::copy(records.begin(), records.begin() + 256, this->records)
+    std::copy(records.begin(), records.begin() + 256, this->records);
     lock_byte = 0;
   }
 
